ab92e1fc636ec1df06652a447982239eee2ff1b0
Serik Serikbay <bsserikbay@gmail.com>
2022-05-16 16:19:13 +0600

    Add project files.

---
+++ WebApplication1/FileLoggerExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication1
+{
+    public static class FileLoggerExtensions
+    {
+        public static ILoggerFactory AddFile(this ILoggerFactory factory, string filePath)
+        {
+            factory.AddProvider(new FileLoggerProvider(filePath));
+            return factory;
+        }
+    }
+}
---
+++ WebApplication1/Program.cs
@@ -0,0 +1,32 @@
+
+using WebApplication1;
+
+var builder = WebApplication.CreateBuilder();
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+var logger = app.Services.GetRequiredService<ILoggerFactory>();
+logger.AddFile(Path.Combine(Directory.GetCurrentDirectory(), "logs"));
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
